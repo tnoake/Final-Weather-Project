2b730a0f5653fae4f4c7f26a683c588318989e2b
tnoake <154478076+tnoake@users.noreply.github.com>
2024-01-31 15:21:35 +1100

    API added

--- SRC/index.js
+++ SRC/index.js
@@ -1,8 +1,25 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = response.data.temperature.current;
+temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+let apiKey = "b58a2f047af526to478d86be21c3e75d";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
     let cityElement = document.querySelector("#weather-app-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
